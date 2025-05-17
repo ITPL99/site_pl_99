3190416ccbc9b64d502b9d94318662698ec6e2c2
Alex Solo <89515014507@mail.ru>
2025-05-17 12:11:36 +0600

    create for empty project

---
+++ src/main/java/com/example/site_pl_99/SitePl99Application.java
@@ -0,0 +1,13 @@
+package com.example.site_pl_99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitePl99Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitePl99Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/site_pl_99/SitePl99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.site_pl_99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitePl99ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
