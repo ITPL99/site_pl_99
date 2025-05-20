709f81df3c1665f821dce113cb445af1b4424d5f
Meslim <AkeSweezy@gmail.com>
2025-05-20 17:48:25 +0600

    i add swagger

---
+++ src/main/java/com/example/site_pl_99/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.site_pl_99.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI aip(){
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                                new Server().url("http://localhost:8081")
+                        )
+                ).info(
+                        new Info().title("site pl99 Api")
+                );
+    }
+
+}
