a919b52e824ad09af9bb6850f916bd55990ee02b
emir <aitbemr@gmail.com>
2025-05-27 15:01:34 +0600

    -Added documentation wit  Swagger

--- src/main/java/com/example/site_pl_99/controller/UserController.java
+++ src/main/java/com/example/site_pl_99/controller/UserController.java
@@ -3,10 +3,12 @@ package com.example.site_pl_99.controller;
 import com.example.site_pl_99.dto.UserDtoRequestRegister;
 import com.example.site_pl_99.dto.UserDtoResponse;
 import com.example.site_pl_99.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "Пользователь")
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -15,17 +17,26 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+    @Operation(
+            summary = "Вернуть список всех пользователей",
+            description = "Возвращает список всех зарегистрированных пользователей, включая их Id, имена и роли."
+    )
     @GetMapping("/all")
     public List<UserDtoResponse> getAllUsers() {
         return userService.getAll().stream().map(UserDtoResponse::new).toList();
     }
-
+    @Operation(
+            summary = "Добавить нового пользователя",
+            description = "Добавляет нового пользователя: требует имя, пароль и роль."
+    )
     @PostMapping("/add")
     public UserDtoResponse addNewUser(@RequestBody UserDtoRequestRegister userDtoRequestRegister) {
         return new UserDtoResponse( userService.save(userDtoRequestRegister));
     }
-
+    @Operation(
+            summary = "Вернуть пользователя по логину",
+            description = "Возвращает информацию о пользователе по указанному логину(имени пользователя) "
+    )
     @PostMapping("/get-login")
     public UserDtoResponse getUserByLogin(@RequestParam String username){
         return new UserDtoResponse(userService.getByUsername(username));
