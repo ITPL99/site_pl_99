2a726ee9a9659fab3fda6f45d09877d9412dfe78
Sweezy <dautov.292009@gmail.com>
2025-06-03 20:32:00 +0600

    fixed problems with swagger

--- src/main/java/com/example/site_pl_99/aspect/AdviceController.java
+++ src/main/java/com/example/site_pl_99/aspect/AdviceController.java
@@ -2,6 +2,7 @@ package com.example.site_pl_99.aspect;
 
 import com.example.site_pl_99.excaption.BaseException;
 import com.example.site_pl_99.utils.Internalization;
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Locale;
 
+@Hidden
 @ControllerAdvice
 public class AdviceController {
     private final Internalization internalization;
